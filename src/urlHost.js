55e7848603e814d053d16ae9ed1fb6588e36cc08
rickReachReporting <rick@reachreporting.com>
2024-06-19 21:25:24 -0700

    update new backend url

--- src/urlHost.js
+++ src/urlHost.js
@@ -1,4 +1,4 @@
 export const URL_HOST =
   process.env.NODE_ENV === "production"
-    ? "https://test-proxy.megancindric.workers.dev/?targetUrl=http://cibus-planning-backend-env.eba-csys5kdh.us-west-2.elasticbeanstalk.com"
+    ? "https://cibus-planning-backend-1f18e73b11ef.herokuapp.com"
     : "http://127.0.0.1:8000";
