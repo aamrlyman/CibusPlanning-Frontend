d699c6873c24ccf7867162da31bb366ad408b0ba
rickReachReporting <rick@reachreporting.com>
2024-06-19 21:37:16 -0700

    update url

--- src/urlHost.js
+++ src/urlHost.js
@@ -1,4 +1,2 @@
 export const URL_HOST =
-  process.env.NODE_ENV === "production"
-    ? "https://cibus-planning-backend-1f18e73b11ef.herokuapp.com"
-    : "http://127.0.0.1:8000";
+  "https://cibus-planning-backend-1f18e73b11ef.herokuapp.com";
