e3ff06c0428d4613ef4bd85d4221200537aa4c29
rickReachReporting <rick@reachreporting.com>
2024-08-09 21:54:20 -0700

    update url to use the lambda url

--- src/urlHost.js
+++ src/urlHost.js
@@ -1,4 +1,4 @@
 export const URL_HOST =
   process.env.NODE_ENV === "production"
-    ? "https://test-proxy.megancindric.workers.dev/?targetUrl=http://cibus-planning-backend-env.eba-csys5kdh.us-west-2.elasticbeanstalk.com"
+    ? "https://uos0rda7x5.execute-api.us-west-2.amazonaws.com/production"
     : "http://127.0.0.1:8000";
