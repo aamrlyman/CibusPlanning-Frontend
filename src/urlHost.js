b3b9c89133151d441c189e92869f972ee5fc0838
rickReachReporting <rick@reachreporting.com>
2024-10-19 08:51:08 -0700

    update host

--- src/urlHost.js
+++ src/urlHost.js
@@ -1,4 +1,4 @@
 export const URL_HOST =
   process.env.NODE_ENV === "production"
-    ? "https://uos0rda7x5.execute-api.us-west-2.amazonaws.com/production"
+    ? "https://aamrlyman.pythonanywhere.com"
     : "http://127.0.0.1:8000";
